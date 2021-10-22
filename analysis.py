43acee0dd8c005246535bcf728139e320ef35baf
Stephen <stephenacrimi@gmail.com>
2021-10-22 17:18:24 -0500

    created analysis on the number of counties each candidate was listed, total votes for each candidate and the number of counties that each candidate won to show evidence of gerrymandering

--- analysis.py
+++ analysis.py
@@ -4,18 +4,43 @@ reader = csv.reader(open('Data/president_county_candidate.csv'))
 
 next(reader)
 
+#going to find number of counties that included each candidate
+candidates_and_parties = {}
+candidates_votes = {}
+candidates_wins = {}
+total_counties = 0 
 
-candidates_and_parties = []
 for row in reader:
-    candidates_and_parties.append(row(2))
-    candidates_and_parties.append(row(2))
-    
+    candidate = row[2]
+    votes = int(row[4])
+    total_counties += 1
+
+    if candidate not in candidates_and_parties.keys():
+        candidates_and_parties[candidate] = 0
+        candidates_votes[candidate] = 0
+        candidates_wins[candidate] = 0
+
+    candidates_and_parties[candidate] += 1
+    candidates_votes[candidate] += votes
+
+    if row[5] == "TRUE":
+        candidates_wins[candidate] += 1
         
-print(candidates_and_parties)
+        
+# for candidate in candidates_and_parties:
+#     print(candidate,': ',candidates_wins[candidate])
+
+output= f'''
+=======ELECTION=ANALYSIS======
+Total Counties: {total_counties}
 
 
+'''
 
 
+print(output)
+
+#going to find number of votes for each candidate
 
 
 
